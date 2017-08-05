c9daff211b5968a89613ab7b4cef36147027e048
Michael Jaggers <mgjaggers@gmail.com>
2017-08-04 18:42:07 -0700

    unix compile binary
    changed gcc to g++

--- unix_compile.sh
+++ unix_compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-sudo gcc main.cpp -o run -lOpenCL -I/opt/AMDAPPSDK-3.0/include/ -lstdc++ || { echo 'Compile Failed!'; exit 1; }
+sudo g++ main.cpp -o run -lOpenCL -I/opt/AMDAPPSDK-3.0/include/ -lstdc++ || { echo 'Compile Failed!'; exit 1; }
 ./run
 rm run
