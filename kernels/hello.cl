c3d2fbe898832444a08ffcd076fbd847c7eff753
Michael Jaggers <mgjaggers@gmail.com>
2017-09-14 17:15:33 -0700

    pointer referencing

--- kernels/hello.cl
+++ kernels/hello.cl
@@ -15,3 +15,56 @@ __kernel void hello(__global char* string)
   string[12] = '!';
   string[13] = '\0';
 }
+
+__kernel void goodbye(__global char* string)
+{
+  string[0] = 'G';
+  string[1] = 'o';
+  string[2] = 'o';
+  string[3] = 'd';
+  string[4] = 'b';
+  string[5] = 'y';
+  string[6] = 'e';
+  string[7] = 'W';
+  string[8] = 'o';
+  string[9] = 'r';
+  string[10] = 'l';
+  string[11] = 'd';
+  string[12] = '!';
+  string[13] = '\0';
+}
+
+__kernel void mem_ptr(__global char* string, __global ulong * ptr)
+{
+    ulong test_me[1];
+    test_me[0] = &string[0];
+    ptr[0] = &string[0];
+    //char * print_me = (char *)test_me[0];
+    char * print_me = (char *)ptr[0];
+    //ptr[1] = &string[1];
+    //string[0] = 'h';
+    //string[1] = 'e';
+    //printf("string: %lu\n", &string);
+    //printf("string: %lu\n", *(string));
+    //printf("%lu\n", (char *)ptr[0]);
+    
+    //print_me[0] = 'I';
+    
+    printf("string: %lu\n", &string[0]);
+    printf("string: %s\n", string);
+    
+    //printf("last:\n");
+    //printf("%c\n", *(string));
+    //printf("%c\n", print_me[2]);
+    //&string[0] = (char *)test_me[0];
+}
+
+__kernel void hello_goodbye(__global ulong * dag_ptr, uint n)
+{
+    char * print_me;
+    printf("uint n: %i\n",n);
+    for(int i = 0; i < 2; i++){
+        print_me = (char *)dag_ptr[i];
+        printf("%i: %s\n",i,print_me);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -7,7 +7,13 @@
 
 #define MEM_SIZE (128)
 #define MAX_SOURCE_SIZE (0x100000)
-
+ void CheckError (cl_int error)
+ {
+ 	if (error != CL_SUCCESS) {
+ 		std::cerr << "OpenCL call failed with error " << error << std::endl;
+ 		std::exit (1);
+ 	}
+ }
 int main() {
   cl_device_id device_id = NULL;
   cl_context context = NULL;
@@ -41,7 +47,7 @@ int main() {
   ret = clGetPlatformIDs(0, NULL, &ret_num_platforms);
   platform_id = (cl_platform_id*)malloc(ret_num_platforms * sizeof(cl_platform_id));
   ret = clGetPlatformIDs(ret_num_platforms, platform_id, &ret_num_platforms);
-  ret = clGetDeviceIDs(platform_id[1], CL_DEVICE_TYPE_ACCELERATOR, 1, &device_id, &ret_num_devices);
+  ret = clGetDeviceIDs(platform_id[0], CL_DEVICE_TYPE_GPU, 1, &device_id, &ret_num_devices);
 
   // Get device name
   size_t device_name_size = 1000;
@@ -60,7 +66,15 @@ int main() {
   command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 
   /* Create Memory Buffer */
-  memobj = clCreateBuffer(context, CL_MEM_READ_WRITE,MEM_SIZE * sizeof(char), NULL, &ret);
+  cl_ulong buffer_ptrs[12];
+  cl_mem hello = NULL;
+  cl_mem goodbye = NULL;
+  cl_mem mem_ptr = NULL;
+  cl_mem DAG_ptrs = NULL;
+  hello   = clCreateBuffer(context, CL_MEM_READ_WRITE,MEM_SIZE * sizeof(char), NULL, &ret);
+  goodbye = clCreateBuffer(context, CL_MEM_READ_WRITE,MEM_SIZE * sizeof(char), NULL, &ret);
+  mem_ptr = clCreateBuffer(context, CL_MEM_READ_WRITE, 1 * sizeof(cl_ulong), NULL, &ret);
+  DAG_ptrs= clCreateBuffer(context, CL_MEM_READ_WRITE, 2 * sizeof(cl_ulong), NULL, &ret);
   
   /* Create Kernel Program from the source */
   program = clCreateProgramWithSource(context, 1, (const char **)&source_str,
@@ -70,21 +84,66 @@ int main() {
   ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
   
   /* Create OpenCL Kernel */
-  kernel = clCreateKernel(program, "hello", &ret);
+  cl_kernel kernel_hello = NULL;
+  cl_kernel kernel_goodbye = NULL;
+  cl_kernel kernel_mem_ptr = NULL;
+  cl_kernel print_both = NULL;
+  kernel_hello = clCreateKernel(program, "hello", &ret);
+  kernel_goodbye = clCreateKernel(program, "goodbye", &ret);
+  kernel_mem_ptr = clCreateKernel(program, "mem_ptr", &ret);
+  print_both = clCreateKernel(program, "hello_goodbye", &ret);
   
   /* Set OpenCL Kernel Parameters */
-  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&memobj);
+  ret = clSetKernelArg(kernel_hello, 0, sizeof(cl_mem), (void *)&hello);
+  ret = clSetKernelArg(kernel_goodbye, 0, sizeof(cl_mem), (void *)&goodbye);
+  ret = clSetKernelArg(print_both, 0, sizeof(cl_mem), (void *)&DAG_ptrs);
+  cl_uint test = 2;
+  ret = clSetKernelArg(print_both, 1, sizeof(cl_uint), &test);
    
   /* Execute OpenCL Kernel */
-  ret = clEnqueueTask(command_queue, kernel, 0, NULL,NULL);
-   
+  ret = clEnqueueTask(command_queue, kernel_hello, 0, NULL,NULL);
+  ret = clEnqueueTask(command_queue, kernel_goodbye, 0, NULL,NULL);
+
   /* Copy results from the memory buffer */
-  ret = clEnqueueReadBuffer(command_queue, memobj, CL_TRUE, 0,
+  ret = clEnqueueReadBuffer(command_queue, hello, CL_TRUE, 0,
   MEM_SIZE * sizeof(char),string, 0, NULL, NULL);
    
   /* Display Result */
   puts(string);
+  
+  /* Copy results from the memory buffer */
+  ret = clEnqueueReadBuffer(command_queue, goodbye, CL_TRUE, 0,
+  MEM_SIZE * sizeof(char),string, 0, NULL, NULL);
    
+  /* Display Result */
+  puts(string);
+  
+  // Now, I want to pass cl_mem goodbye to kernel_hello and then 
+  ret = clSetKernelArg(kernel_mem_ptr, 0, sizeof(cl_mem), (void *)&hello);
+  CheckError(ret);
+  ret = clSetKernelArg(kernel_mem_ptr, 1, sizeof(cl_mem), (void *)&mem_ptr);
+  CheckError(ret);
+  ret = clEnqueueTask(command_queue, kernel_mem_ptr, 0, NULL,NULL);
+  CheckError(ret);
+  ret = clEnqueueCopyBuffer(command_queue, mem_ptr, DAG_ptrs, 0, 0 * sizeof(cl_ulong), 
+                            sizeof(cl_ulong), 0, NULL, NULL);
+  
+  ret = clSetKernelArg(kernel_mem_ptr, 0, sizeof(cl_mem), (void *)&goodbye);
+  CheckError(ret);
+  ret = clSetKernelArg(kernel_mem_ptr, 1, sizeof(cl_mem), (void *)&mem_ptr);
+  CheckError(ret);
+  ret = clEnqueueTask(command_queue, kernel_mem_ptr, 0, NULL,NULL);
+  CheckError(ret);
+  ret = clEnqueueCopyBuffer(command_queue, mem_ptr, DAG_ptrs, 0, 1 * sizeof(cl_ulong),
+                            sizeof(cl_ulong), 0, NULL, NULL);
+
+  ret = clEnqueueReadBuffer(command_queue, mem_ptr, CL_TRUE, 0,
+  1 * sizeof(cl_ulong),buffer_ptrs, 0, NULL, NULL);  
+  printf("tada:%lu\n",buffer_ptrs[0]);
+  
+   ret = clEnqueueTask(command_queue, print_both, 0, NULL,NULL);
+
+  printf("final\n");
   /* Finalization */
   ret = clFlush(command_queue);
   ret = clFinish(command_queue);
